2170b937d11a818d34d2eea3f71e93a9dde26579
clucaseh <clucas@everydayhealthinc.com>
2020-04-18 19:15:06 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,16 +27,16 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2a039aaf8aad427eaca9.js"
+    "url": "webpack-runtime-897788c19cd1dc1b8ce9.js"
   },
   {
-    "url": "styles.8b39a2a05d949ac3569f.css"
+    "url": "styles.88e36c7e6ce1a093c774.css"
   },
   {
-    "url": "styles-34371fb3f1cef9f8e92b.js"
+    "url": "styles-46de58125dc52c51d770.js"
   },
   {
-    "url": "commons-036829a102e882718702.js"
+    "url": "commons-a776fe503f505ca84c11.js"
   },
   {
     "url": "netlify-identity-widget-8e6f024872c0d48ae324.js"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "eed21b9ba0781baeea65391a9a964603"
+    "revision": "9595e3d9428e96461b3d619507a92c98"
   },
   {
     "url": "manifest.json",
