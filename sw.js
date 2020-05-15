1ddffecdf19a1ce7bf772feedd82a3130216e005
clucaseh <clucas@everydayhealthinc.com>
2020-05-14 21:21:49 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8ed798aff40b99e5b952.js"
+    "url": "webpack-runtime-fa80d9fe53bfa4ff4242.js"
   },
   {
-    "url": "styles.9d3bc2a34e9ba25f543d.css"
+    "url": "styles.b32e55e840b744c8b161.css"
   },
   {
     "url": "styles-cf78e1705ae193b7df97.js"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "452d0e1e28b974ca8bfc1a26fde4113e"
+    "revision": "a26b065c4b28e00548eafcb56c8c21c4"
   },
   {
     "url": "manifest.json",
